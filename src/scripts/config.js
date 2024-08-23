8a52d6e602df640a58b881e95c90eab1ca1d51fe
hoangnv170752 <hoang.nv.ral@gmail.com>
2024-08-23 23:26:03 +0700

    fix: fix config file

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,8 +1,10 @@
 const Config = {
-  ROSBRIDGE_SERVER_IP: "192.168.1.145",
-  ROSBRIDGE_SERVER_PORT: "9090",
+  ROSBRIDGE_SERVER_IP: "210.211.96.135",
+  ROSBRIDGE_SERVER_PORT: "9680",
   RECONNECTION_TIMER: 3000,
-  CMD_VEL_TOPIC: "/turtle1/cmd_vel",
+  CMD_VEL_TOPIC: "/cmd_vel",
+  ODOM_TOPIC: "/odom",
+  POSE_TOPIC: "/robot_pose",
 };
 
 export default Config;
